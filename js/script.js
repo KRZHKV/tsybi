c7c5bde63593708cc785d5ccc97149ec86a66cc3
Kirill Kruzhkov <Kruzhkovll@gmail.com>
2020-03-07 16:08:50 +0300

    rebuild menu, slider, footer blocks

--- js/script.js
+++ js/script.js
@@ -1,10 +1,22 @@
-// $(document).ready(function() {
-//     $('.slider').slick({
-//         infinite: true,
-//         slidesToShow: 3,
-//         slidesToScroll: 1,
-//         dots: true
-//       });
-// });
+$(document).ready(function() {
+    $('.slider').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+		dots: true,
+		centerMode: true,
+		dotClass: "slider-dot",
+    	centerPadding: '50px',
+    	prevArrow: "<button class='slider-prev' alt='1'><i class='fas fa-chevron-left'></i></button>",
+    	nextArrow: "<button class='slider-next' alt='1'><i class='fas fa-chevron-right'></i></button>"
+	  });
+	  
+	  let dots = document.querySelectorAll('button[role="tab"]');
+	  dots.forEach((elem) => {
+		elem.textContent = '';
+	  });
+});
+
+
 
 
