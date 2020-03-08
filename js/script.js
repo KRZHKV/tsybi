ea1cd35d3cd19a080bf72d75f9c3095e7ea00d00
Kirill Kruzhkov <Kruzhkovll@gmail.com>
2020-03-08 11:57:20 +0300

    massive update

--- js/script.js
+++ js/script.js
@@ -4,11 +4,22 @@ $(document).ready(function() {
         slidesToShow: 3,
         slidesToScroll: 1,
 		dots: true,
-		centerMode: true,
+		autoplay: true,
+		autoplaySpeed: 2000,
 		dotClass: "slider-dot",
     	centerPadding: '50px',
     	prevArrow: "<button class='slider-prev' alt='1'><i class='fas fa-chevron-left'></i></button>",
-    	nextArrow: "<button class='slider-next' alt='1'><i class='fas fa-chevron-right'></i></button>"
+		nextArrow: "<button class='slider-next' alt='1'><i class='fas fa-chevron-right'></i></button>",
+		responsive: [
+			{
+			  breakpoint: 768,
+			  settings: {
+				dots: false,
+				slidesToShow: 1
+			  }
+			},
+			
+		  ]
 	  });
 	  
 	  let dots = document.querySelectorAll('button[role="tab"]');
