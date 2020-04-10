620f3e357cb068da5f48bde95b8a9a7c5dfd3d4b
Kirill Kruzhkov <Kruzhkovll@gmail.com>
2020-04-10 11:44:24 +0300

    блок доставка, редизайн

--- mail.php
+++ mail.php
@@ -8,34 +8,23 @@ $name = $_POST['name'];
 $phone = $_POST['phone'];
 $email = $_POST['email'];
 $message = $_POST['message'];
-
-//$mail->SMTPDebug = 3;                               // Enable verbose debug output
-
-$mail->isSMTP();                                      // Set mailer to use SMTP
-$mail->Host = 'smtp.strana-03.ru';  																							// Specify main and backup SMTP servers
-$mail->SMTPAuth = true;                               // Enable SMTP authentication
+                           // Enable verbose debug output
+                                     // Set mailer to use SMTP                              // Enable SMTP authentication
 $mail->Username = 'question@strana-03.ru'; // Ваш логин от почты с которой будут отправляться письма
-$mail->Password = 'Qwerty123456'; // Ваш пароль от почты с которой будут отправляться письма
-$mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
-$mail->Port = 465; // TCP port to connect to / этот порт может отличаться у других провайдеров
+$mail->Password = 'Qwerty123456'; // Ваш пароль от почты с которой будут отправляться письма                          // Enable TLS encryption, `ssl` also accepted
 
 $mail->setFrom('question@strana-03.ru'); // от кого будет уходить письмо?
-$mail->addAddress('kirkruzhkov@yandex.ru');     // Кому будет уходить письмо 
-//$mail->addAddress('ellen@example.com');               // Name is optional
-//$mail->addReplyTo('info@example.com', 'Information');
-//$mail->addCC('cc@example.com');
-//$mail->addBCC('bcc@example.com');
-//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
-//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
-$mail->isHTML(true);                                  // Set email format to HTML
+$mail->addAddress('summers92@mail.ru');
+$mail->addAddress('kirkruzhkov@yandex.ru'); // Кому будет уходить письмо                                 
+$mail->isHTML(true);// Set email format to HTML
 
-$mail->Subject = 'Заявка с тестового сайта';
-$mail->Body    = '' .$name . ' оставил заявку, его телефон ' .$phone. '<br>Почта этого пользователя: ' .$email. '<br>Сообщение пользователя: ' .$message;
+$mail->Subject = 'Заявка с сайта Strana-03';
+$mail->Body    = 'Пользователь: ' .$name . ' оставил заявку, на номер ' .$phone. '<br>Почта этого пользователя: ' .$email. '<br>Сообщение пользователя: ' .$message;
 $mail->AltBody = '';
 
 if(!$mail->send()) {
     echo 'Error';
 } else {
-    header('location: thank-you.html');
+    header('location: thanks.html');
 }
 ?>
